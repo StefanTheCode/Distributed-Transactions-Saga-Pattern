6be90ce0c6fd7225832dd5e65b055cfdf6e0503a
S673659 <Stefan.X.Djokic@kp.org>
2022-06-08 23:01:50 +0200

    Domain started

---
+++ TripBooking/HotelService.Domain/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿using Saga.Shared.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelService.Domain.Entities
+{
+    public class Booking : AuditableEntity
+    {
+
+    }
+}
